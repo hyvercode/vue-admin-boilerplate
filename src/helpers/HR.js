22686bbb50838ea24ba7364664f6f1117ceef56d
mohirwanh <mohirwan@gmail.com>
2022-04-12 12:18:33 +0700

    refactoring mih data table

---
+++ src/helpers/HR.js
@@ -0,0 +1,21 @@
+const HR={
+    HR: "/HR",
+    COMPANIES: "/HR/companies",
+    COMPANY_CREATE: "/HR/company",
+    COMPANY_UPDATE: "/HR/company/update",
+    BRANCH: "/HR/branches",
+    BRANCH_CREATE: "/HR/branch",
+    BRANCH_UPDATE: "/HR/branch/update",
+    EMPLOYEES: "/HR/employees",
+    EMPLOYEES_FAMILY: "/HR/employees/family",
+    EMPLOYEE_CREATE: "/HR/employee",
+    EMPLOYEE_UPDATE: "/HR/employee/update",
+    UNIT: "/HR/unit",
+    UNIT_MEMBER: "/HR/unit/member",
+    JOB_POSITION: "/HR/job/position",
+    JOB_TITLE: "/HR/job/title",
+    ATTENDANCES: "/HR/attendances",
+    ATTENDANCE_TMP: "/HR/attendance/tmp",
+}
+
+export default HR;
