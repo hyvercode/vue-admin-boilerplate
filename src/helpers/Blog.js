60f9960f3e7ddba6b8de1ca2adc34028e14c3dd2
mohirwanh <mohirwan@gmail.com>
2022-05-12 09:44:58 +0700

    refactoring

---
+++ src/helpers/Blog.js
@@ -0,0 +1,19 @@
+const BLOG={
+    BLOG: "/blog",
+    BLOG_DETAIL: "/blog/:id",
+    BLOG_CATEGORY: "/blog/category/:id",
+    BLOG_TAG: "/blog/tag/:id",
+    BLOG_SEARCH: "/blog/search/:id",
+    BLOG_ARCHIVE: "/blog/archive/:id",
+    BLOG_ARCHIVE_DETAIL: "/blog/archive/:id/:id",
+    BLOG_ARCHIVE_CATEGORY: "/blog/archive/category/:id",
+    BLOG_ARCHIVE_TAG: "/blog/archive/tag/:id",
+    BLOG_ARCHIVE_SEARCH: "/blog/archive/search/:id",
+    BLOG_ARCHIVE_YEAR: "/blog/archive/:id/:id",
+    BLOG_ARCHIVE_MONTH: "/blog/archive/:id/:id",
+    BLOG_ARCHIVE_DAY: "/blog/archive/:id/:id",
+    BLOG_ARCHIVE_YEAR_MONTH: "/blog/archive/:id/:id/:id",
+    BLOG_ARCHIVE_YEAR_MONTH_DAY: "/blog/archive/:id/:id/:id/:id",
+}
+
+export default BLOG;
