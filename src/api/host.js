47d50528cca9e7c9eeaf8454e862c22c2b0857b8
mohirwanh <mohirwan@gmail.com>
2022-06-09 16:06:40 +0700

    deploy

--- src/api/host.js
+++ src/api/host.js
@@ -1,4 +1,5 @@
-const API_V1 = "http://localhost:8888/api/v1"
+const API_V1 = "https://api-he.solusione.id/api/v1"
+// const API_V1 = "http://localhost:8888/api/v1"
 export default {
     API_V1
 };
