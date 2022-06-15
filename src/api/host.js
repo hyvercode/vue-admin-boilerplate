60a7ff960a47db2686ed78419653ce520dc7edf8
mohirwanh <mohirwan@gmail.com>
2022-06-15 15:57:08 +0700

    kanban

--- src/api/host.js
+++ src/api/host.js
@@ -1,5 +1,5 @@
-// const API_V1 = "https://api-hr.solusione.id/api/v1"
-const API_V1 = "http://localhost:8888/api/v1"
+const API_V1 = "https://api-hr.solusione.id/api/v1"
+//const API_V1 = "http://localhost:8888/api/v1"
 export default {
     API_V1
 };
